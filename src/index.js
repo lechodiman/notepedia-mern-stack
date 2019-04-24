4a39e15191cb57ce888df6e755a296004e21e405
lechodiman <lechodiman@uc.cl>
2019-04-23 20:49:41 -0400

    Fix import in index

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 import { store, history } from "./redux/store";
+import { getUser } from "./redux/actions/actions";
 
 if (localStorage.Auth) {
   store.dispatch({ type: "SET_USER", user: JSON.parse(localStorage.Auth) });
