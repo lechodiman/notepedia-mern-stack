0ef32675a1a2165c7392c226096f31a7d89043d9
lechodiman <lechodiman@uc.cl>
2019-04-23 20:33:06 -0400

    Add Editor, ArticleView and Profile

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { loadArticles } from "./../redux/actions/actions";
 import AsideFeed from "./AsideFeed";
+
 const mapStateToProps = state => {
   return {
     articles: state.articles.articles
