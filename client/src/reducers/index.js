39b980d70e8b7506cc32ee457222d78b3cfbccad
lechodiman <lechodiman@uc.cl>
2019-05-08 18:15:10 -0400

    Alert Reducer, Actino and Types

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from "redux";
 import articles from "./articleReducer";
 import authUser from "./authReducer";
+import alert from "./alertReducer";
 
 export default combineReducers({
   articles,
   authUser,
-  common
+  alert
 });
