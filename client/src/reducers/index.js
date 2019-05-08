4818547c4d35e1046df1546bde8da3d874f05235
lechodiman <lechodiman@uc.cl>
2019-05-08 18:06:23 -0400

    Set Up Store

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,14 +1,9 @@
 import { combineReducers } from "redux";
 import articles from "./articleReducer";
 import authUser from "./authReducer";
-import common from "./commonReducer";
-import { connectRouter } from "connected-react-router";
 
-export default history => {
-  return combineReducers({
-    router: connectRouter(history),
-    articles,
-    authUser,
-    common
-  });
-};
+export default combineReducers({
+  articles,
+  authUser,
+  common
+});
