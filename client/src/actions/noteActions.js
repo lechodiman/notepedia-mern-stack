75f5766f0a45816331ec0120687e1c9f07ed8dbd
lechodiman <lechodiman@uc.cl>
2019-05-12 12:04:34 -0400

    Add all mighty console.log

--- client/src/actions/noteActions.js
+++ client/src/actions/noteActions.js
@@ -1,13 +1,9 @@
 import axios from "axios";
 import { LOAD_NOTES } from "./types";
 
-// const url =
-//   process.env.NODE_ENV === "production"
-//     ? "/api/"
-//     : "http://localhost:5000/api/";
-
 // Loads all notes from the DB
 export const loadNotes = () => async dispatch => {
-    const res = await axios.get("/api/notes");
-    dispatch({ type: LOAD_NOTES, payload: res.data});
+  const res = await axios.get("/api/notes");
+  console.log(res);
+  dispatch({ type: LOAD_NOTES, payload: res.data });
 };
