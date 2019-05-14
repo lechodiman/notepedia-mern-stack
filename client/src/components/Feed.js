56ca9c5a2d4cdac443776795b005c8e91b58c683
lechodiman <lechodiman@uc.cl>
2019-05-14 09:58:16 -0400

    Fix key warning

--- client/src/components/Feed.js
+++ client/src/components/Feed.js
@@ -4,10 +4,9 @@ import NotePost from "./NotePostView";
 import { connect } from "react-redux";
 
 const Feed = ({ allNotes, loadNotes }) => {
-
   useEffect(() => {
     loadNotes();
-  }, []);
+  }, [loadNotes]);
 
   const feedLayout = {
     display: "grid",
@@ -20,14 +19,16 @@ const Feed = ({ allNotes, loadNotes }) => {
 
   // Maps notes retrieved from the DB into NotePost components to display in feed
   const displayNotes = allNotes.map(note => (
-    <NotePost title={note.title} description={note.description} />
+    <NotePost
+      title={note.title}
+      description={note.description}
+      key={note._id}
+    />
   ));
 
   return (
     <div style={feedLayout}>
-      <div style={feedPost}>
-        {displayNotes}
-      </div>
+      <div style={feedPost}>{displayNotes}</div>
     </div>
   );
 };
