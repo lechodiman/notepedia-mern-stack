947b88a08beb7ae7b641458ee2645c3b913a7822
Cesar Meneses <cnmeneses@uc.cl>
2019-06-28 19:26:17 -0400

    add weights for text indexes

--- models/Note.js
+++ models/Note.js
@@ -2,59 +2,55 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 let NoteSchema = new Schema({
-  author: {
-    type: Schema.Types.ObjectId,
-    ref: "user"
-  },
-  text: { type: String, required: true },
-  title: { type: String, required: true },
-  description: String,
-  feature_img: String,
-  likes: [
-    {
-      user: {
-        type: Schema.Types.ObjectId,
-        ref: "users"
-      }
-    }
-  ],
-  comments: [
-    {
-      author: {
+    author: {
         type: Schema.Types.ObjectId,
         ref: "user"
-      },
-      text: { type: String, required: true },
-      name: {
-        type: String
-      },
-      avatar: {
-        type: String
-      },
-      date: {
-        type: Date,
-        default: Date.now
-      }
-    }
-  ]
+    },
+    text: { type: String, required: true },
+    title: { type: String, required: true },
+    description: String,
+    feature_img: String,
+    likes: [{
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: "users"
+        }
+    }],
+    comments: [{
+        author: {
+            type: Schema.Types.ObjectId,
+            ref: "user"
+        },
+        text: { type: String, required: true },
+        name: {
+            type: String
+        },
+        avatar: {
+            type: String
+        },
+        date: {
+            type: Date,
+            default: Date.now
+        }
+    }]
 });
 NoteSchema.methods.clap = function() {
-  this.claps++;
-  return this.save();
+    this.claps++;
+    return this.save();
 };
 NoteSchema.methods.comment = function(comment) {
-  this.comments.push(comment);
-  return this.save();
+    this.comments.push(comment);
+    return this.save();
 };
 NoteSchema.methods.addAuthor = function(authorId) {
-  this.author = authorId;
-  return this.save();
+    this.author = authorId;
+    return this.save();
 };
 NoteSchema.methods.getUserNotes = function(authorId) {
-  Note.find({ author: authorId }).then(notes => {
-    return notes;
-  });
+    Note.find({ author: authorId }).then(notes => {
+        return notes;
+    });
 };
-NoteSchema.index({ text: "text", title: "text", description: "text" });
+NoteSchema.index({ title: "text", description: "text", text: "text" }, { weights: { title: 20, description: 5 }, name: "TextIndex" });
 
 module.exports = Note = mongoose.model("note", NoteSchema);
