72cb623e119b3ec6255d782ca2ed0dfac4edac9e
Luis Chodiman <lechodiman@uc.cl>
2019-06-28 17:57:11 -0700

    Fix class to className typos

--- client/src/components/note/CommentForm.js
+++ client/src/components/note/CommentForm.js
@@ -7,8 +7,8 @@ const CommentForm = ({ noteId, addComment }) => {
   const [text, setText] = useState("");
 
   return (
-    <div class="post-form">
-      <div class="bg-primary p-2 text-white">
+    <div className="post-form">
+      <div className="bg-primary p-2 text-white">
         <h3 className="lead">Leave a Comment</h3>
       </div>
       <form
@@ -30,7 +30,7 @@ const CommentForm = ({ noteId, addComment }) => {
             className="form-control"
           />
         </div>
-        <input type="submit" class="btn btn-primary my-1" value="Submit" />
+        <input type="submit" className="btn btn-primary my-1" value="Submit" />
       </form>
     </div>
   );
