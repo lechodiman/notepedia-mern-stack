a69f3b9d4af569fe8649f1ee786bad7131d9b282
Luis Chodiman <lechodiman@uc.cl>
2019-06-29 16:16:00 -0700

    Update 4 spaces to 2

--- models/Note.js
+++ models/Note.js
@@ -2,54 +2,61 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 let NoteSchema = new Schema({
-    author: {
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "user"
+  },
+  text: { type: String, required: true },
+  title: { type: String, required: true },
+  description: String,
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+      }
+    }
+  ],
+  comments: [
+    {
+      author: {
         type: Schema.Types.ObjectId,
         ref: "user"
-    },
-    text: { type: String, required: true },
-    title: { type: String, required: true },
-    description: String,
-    likes: [{
-        user: {
-            type: Schema.Types.ObjectId,
-            ref: "users"
-        }
-    }],
-    comments: [{
-        author: {
-            type: Schema.Types.ObjectId,
-            ref: "user"
-        },
-        text: { type: String, required: true },
-        name: {
-            type: String
-        },
-        avatar: {
-            type: String
-        },
-        date: {
-            type: Date,
-            default: Date.now
-        }
-    }],
-    date: {
+      },
+      text: { type: String, required: true },
+      name: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
         type: Date,
         default: Date.now
+      }
     }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
 NoteSchema.methods.comment = function(comment) {
-    this.comments.push(comment);
-    return this.save();
+  this.comments.push(comment);
+  return this.save();
 };
 NoteSchema.methods.addAuthor = function(authorId) {
-    this.author = authorId;
-    return this.save();
+  this.author = authorId;
+  return this.save();
 };
 NoteSchema.methods.getUserNotes = function(authorId) {
-    Note.find({ author: authorId }).then(notes => {
-        return notes;
-    });
+  Note.find({ author: authorId }).then(notes => {
+    return notes;
+  });
 };
-NoteSchema.index({ title: "text", description: "text", text: "text" }, { weights: { title: 20, description: 5 }, name: "TextIndex" });
+NoteSchema.index(
+  { title: "text", description: "text", text: "text" },
+  { weights: { title: 20, description: 5 }, name: "TextIndex" }
+);
 
 module.exports = Note = mongoose.model("note", NoteSchema);
--- routes/api/notes.js
+++ routes/api/notes.js
@@ -13,297 +13,309 @@ const User = require("../../models/User");
 // @desc     Create a note
 // @access   Private
 router.post(
-    "/", [
-        multipartWare,
-        auth, [
-            check("text", "Text is required")
-            .not()
-            .isEmpty()
-        ]
-    ],
-    async(req, res) => {
-        try {
-            const user = await User.findById(req.user.id).select("-password");
-            const { text, title, claps, description } = req.body;
+  "/",
+  [
+    multipartWare,
+    auth,
+    [
+      check("text", "Text is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+      const { text, title, claps, description } = req.body;
 
-            let noteParameters = {
-                text,
-                title,
-                claps,
-                description,
-                feature_img: "",
-                author: req.user.id
-            };
+      let noteParameters = {
+        text,
+        title,
+        claps,
+        description,
+        feature_img: "",
+        author: req.user.id
+      };
 
-            if (req.files && req.files.image) {
-                const uploadedImage = cloudinary.uploader.upload(req.files.image.path, {
-                    resource_type: "image",
-                    eager: [{ effect: "sepia" }]
-                });
+      if (req.files && req.files.image) {
+        const uploadedImage = cloudinary.uploader.upload(req.files.image.path, {
+          resource_type: "image",
+          eager: [{ effect: "sepia" }]
+        });
 
-                noteParameters = {
-                    ...noteParameters,
-                    feature_img: uploadedImage.url
-                };
-            }
+        noteParameters = {
+          ...noteParameters,
+          feature_img: uploadedImage.url
+        };
+      }
 
-            const note = new Note(noteParameters);
+      const note = new Note(noteParameters);
 
-            const savedNote = await note.save();
+      const savedNote = await note.save();
 
-            const noteWithAuthor = savedNote.populate("author", "-password");
+      const noteWithAuthor = savedNote.populate("author", "-password");
 
-            res.json(noteWithAuthor);
-        } catch (err) {
-            console.error(err.message);
-            res.status(500).send("Server Error");
-        }
+      res.json(noteWithAuthor);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
     }
+  }
 );
 
 // @route    GET api/notes
 // @desc     Get all notes
 // @access   Private
-router.get("/", async(req, res) => {
-    try {
-        const notes = await Note.find()
-            .populate("author", "-password")
-            .sort({ date: -1 });
-        res.json(notes);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send("Server Error");
-    }
+router.get("/", async (req, res) => {
+  try {
+    const notes = await Note.find()
+      .populate("author", "-password")
+      .sort({ date: -1 });
+    res.json(notes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 // @route    GET api/notes/search
 // @desc     Search for notes given a text
 // @access   Private
 
-router.get("/search", auth, async(req, res) => {
-    try {
-        const perPage = 5;
-        const notes = await Note.find({
-            $text: {
-                $search: req.body.text
-            }
-        }, { score: { $meta: "textScore" } }, {
-            limit: perPage,
-            skip: perPage * req.body.page,
-            sort: { score: { $meta: "textScore" } }
-        });
-        res.json(notes);
-    } catch (err) {
-        throw err;
-        res.status(500).send("Server Error");
-    }
+router.get("/search", auth, async (req, res) => {
+  try {
+    const perPage = 5;
+    const notes = await Note.find(
+      {
+        $text: {
+          $search: req.body.text
+        }
+      },
+      { score: { $meta: "textScore" } },
+      {
+        limit: perPage,
+        skip: perPage * req.body.page,
+        sort: { score: { $meta: "textScore" } }
+      }
+    );
+    res.json(notes);
+  } catch (err) {
+    throw err;
+    res.status(500).send("Server Error");
+  }
 });
 
 // @route    GET api/notes/:id
 // @desc     Get note by ID
 // @access   Public
-router.get("/:id", async(req, res) => {
-    try {
-        const note = await Note.findById(req.params.id)
-            .populate("author", "-password")
-            .populate("comments.author");
+router.get("/:id", async (req, res) => {
+  try {
+    const note = await Note.findById(req.params.id)
+      .populate("author", "-password")
+      .populate("comments.author");
 
-        if (!note) {
-            return res.status(404).json({ message: "Note not found" });
-        }
-
-        res.json(note);
-    } catch (err) {
-        console.error(err.message);
-        if (err.kind === "ObjectId") {
-            return res.status(404).json({ message: "Note not found" });
-        }
+    if (!note) {
+      return res.status(404).json({ message: "Note not found" });
     }
+
+    res.json(note);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ message: "Note not found" });
+    }
+  }
 });
 
 // @route    DELETE api/notes/:id
 // @desc     Delete a note
 // @access   Private
-router.delete("/:id", auth, async(req, res) => {
-    try {
-        const note = await Note.findById(req.params.id);
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const note = await Note.findById(req.params.id);
 
-        if (!note) {
-            return res.status(404).json({ message: "Note not found" });
-        }
-
-        // Check user
-        if (note.author.toString() !== req.user.id) {
-            return res.status(401).json({ message: "User not authorized" });
-        }
-
-        await note.remove();
-
-        res.json({ message: "Note removed" });
-    } catch (err) {
-        console.error(err.message);
-        if (err.kind === "ObjectId") {
-            return res.status(404).json({ message: "Note not found" });
-        }
+    if (!note) {
+      return res.status(404).json({ message: "Note not found" });
     }
+
+    // Check user
+    if (note.author.toString() !== req.user.id) {
+      return res.status(401).json({ message: "User not authorized" });
+    }
+
+    await note.remove();
+
+    res.json({ message: "Note removed" });
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ message: "Note not found" });
+    }
+  }
 });
 
 // @route    POST api/notes/comment/:id
 // @desc     Comment on a note
 // @access   Private
 router.post(
-    "/comment/:id", [
-        auth, [
-            check("text", "Text is required")
-            .not()
-            .isEmpty()
-        ]
-    ],
-    async(req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-
-        try {
-            const user = await User.findById(req.user.id).select("-password");
-            const note = await Note.findById(req.params.id);
-
-            const newComment = {
-                text: req.body.text,
-                name: user.name,
-                avatar: user.avatar,
-                author: req.user.id
-            };
-
-            note.comments = [newComment, ...note.comments];
-
-            await note.save();
-
-            res.json(note.comments);
-        } catch (err) {
-            console.error(err.message);
-            res.status(500).send("Server Error");
-        }
+  "/comment/:id",
+  [
+    auth,
+    [
+      check("text", "Text is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+      const note = await Note.findById(req.params.id);
+
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        author: req.user.id
+      };
+
+      note.comments = [newComment, ...note.comments];
+
+      await note.save();
+
+      res.json(note.comments);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
 );
 
 // @route    DELETE api/notes/comment/:id/:comment_id
 // @desc     Delete comment
 // @access   Private
-router.delete("/comment/:id/:comment_id", auth, async(req, res) => {
-    try {
-        const note = await Note.findById(req.params.id);
+router.delete("/comment/:id/:comment_id", auth, async (req, res) => {
+  try {
+    const note = await Note.findById(req.params.id);
 
-        // Pull out comment
-        const comment = note.comments.find(
-            comment => comment.id === req.params.comment_id
-        );
+    // Pull out comment
+    const comment = note.comments.find(
+      comment => comment.id === req.params.comment_id
+    );
 
-        // Make sure comment exists
-        if (!comment) {
-            return res.status(404).json({ message: "Comment does not exist" });
-        }
-
-        // Check user
-        if (comment.author.toString() !== req.user.id) {
-            return res.status(401).json({ message: "User not authorized" });
-        }
-
-        // Get remove index
-        const removeIndex = note.comments
-            .map(comment => comment.author.toString())
-            .indexOf(req.user.id);
-
-        note.comments.splice(removeIndex, 1);
-
-        await note.save();
-
-        res.json(note.comments);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send("Server Error");
+    // Make sure comment exists
+    if (!comment) {
+      return res.status(404).json({ message: "Comment does not exist" });
     }
+
+    // Check user
+    if (comment.author.toString() !== req.user.id) {
+      return res.status(401).json({ message: "User not authorized" });
+    }
+
+    // Get remove index
+    const removeIndex = note.comments
+      .map(comment => comment.author.toString())
+      .indexOf(req.user.id);
+
+    note.comments.splice(removeIndex, 1);
+
+    await note.save();
+
+    res.json(note.comments);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 // @route    PATCH api/notes/:id/
 // @desc     Update a note
 // @access   Private
-router.patch("/:id", auth, async(req, res) => {
-    try {
-        const note = await Note.findById(req.params.id);
+router.patch("/:id", auth, async (req, res) => {
+  try {
+    const note = await Note.findById(req.params.id);
 
-        // Check if the note exists
-        if (!note) {
-            return res.status(404).json({ message: "Note not found" });
-        }
-
-        // Check user
-        if (note.author.toString() !== req.user.id) {
-            return res.status(401).json({ message: "User not authorized" });
-        }
-
-        const updatedNote = await Note.findByIdAndUpdate({ _id: req.params.id }, { $set: req.body }, { new: true });
-
-        res.json(updatedNote);
-    } catch (err) {
-        console.log(err);
-        res.status(500).send("Server Error");
+    // Check if the note exists
+    if (!note) {
+      return res.status(404).json({ message: "Note not found" });
     }
+
+    // Check user
+    if (note.author.toString() !== req.user.id) {
+      return res.status(401).json({ message: "User not authorized" });
+    }
+
+    const updatedNote = await Note.findByIdAndUpdate(
+      { _id: req.params.id },
+      { $set: req.body },
+      { new: true }
+    );
+
+    res.json(updatedNote);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Server Error");
+  }
 });
 
 // @route    PUT api/notes/like/:id
 // @desc     Like a note
 // @access   Private
-router.put("/like/:id", auth, async(req, res) => {
-    try {
-        const note = await Note.findById(req.params.id);
+router.put("/like/:id", auth, async (req, res) => {
+  try {
+    const note = await Note.findById(req.params.id);
 
-        // Check if the note has already been liked
-        if (
-            note.likes.filter(like => like.user.toString() === req.user.id).length > 0
-        ) {
-            return res.status(400).json({ msg: "Note already liked" });
-        }
-
-        note.likes.unshift({ user: req.user.id });
-
-        await note.save();
-
-        res.json(note.likes);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send("Server Error");
+    // Check if the note has already been liked
+    if (
+      note.likes.filter(like => like.user.toString() === req.user.id).length > 0
+    ) {
+      return res.status(400).json({ msg: "Note already liked" });
     }
+
+    note.likes.unshift({ user: req.user.id });
+
+    await note.save();
+
+    res.json(note.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 // @route    PUT api/notes/unlike/:id
 // @desc     Unlike a note
 // @access   Private
-router.put("/unlike/:id", auth, async(req, res) => {
-    try {
-        const note = await Note.findById(req.params.id);
+router.put("/unlike/:id", auth, async (req, res) => {
+  try {
+    const note = await Note.findById(req.params.id);
 
-        // Check if the note has already been liked
-        if (
-            note.likes.filter(like => like.user.toString() === req.user.id).length ===
-            0
-        ) {
-            return res.status(400).json({ msg: "Note has not yet been liked" });
-        }
-
-        // Get remove index
-        const removeIndex = note.likes
-            .map(like => like.user.toString())
-            .indexOf(req.user.id);
-
-        note.likes.splice(removeIndex, 1);
-
-        res.json(note.likes);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send("Server Error");
+    // Check if the note has already been liked
+    if (
+      note.likes.filter(like => like.user.toString() === req.user.id).length ===
+      0
+    ) {
+      return res.status(400).json({ msg: "Note has not yet been liked" });
     }
+
+    // Get remove index
+    const removeIndex = note.likes
+      .map(like => like.user.toString())
+      .indexOf(req.user.id);
+
+    note.likes.splice(removeIndex, 1);
+
+    res.json(note.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 module.exports = router;
